dac6be655b757c94386e70e6ee262d771771d422
David Badura <david.badura@i22.de>
2017-02-28 20:57:04 +0100

    add button & handle click -> save url in local storage

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,12 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
+/*
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+  */
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,39 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+chrome.extension.sendMessage({}, function (response) {
+  const readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      init();
+    }
+  }, 10);
 });
+
+function init() {
+  console.log('init');
+
+  const sidebar = document.querySelector('.discussion-sidebar-item.sidebar-notifications');
+
+  const button = document.createElement('button');
+  button.innerText = 'test';
+  button.onclick = handleClick;
+
+  sidebar.appendChild(button);
+}
+
+function handleClick() {
+  chrome.storage.sync.get('todos', items => {
+    const todos = items.todos || [];
+    const url = location.href;
+    console.log(items);
+    console.log(todos);
+
+    if (todos.indexOf(url) === -1) {
+      todos.push(url);
+
+      chrome.storage.sync.set({
+        'todos': todos
+      });
+    }
+
+    console.log(todos);
+  });
+}

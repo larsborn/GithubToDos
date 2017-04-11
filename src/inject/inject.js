40266b28d5cbfa95890a897f24ff5236094d46c6
Lars Wallenborn <lars@wallenborn.net>
2017-04-11 21:33:39 +0200

    fixes typo

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -312,7 +312,7 @@ function removeTodo(todo) {
   });
 }
 
-/* extract informations */
+/* extract information */
 
 function extractTodoInformation() {
   return {
@@ -353,11 +353,11 @@ function extractParticipants() {
 function getCurrentTimestamp() {
   const now = new Date();
   return now.getFullYear()
-    + "-" + ("0" + (now.getMonth() + 1)).slice(-2)
-    + "-" + ("0" + now.getDate()).slice(-2)
-    + " " + ("0" + now.getHours()).slice(-2)
-    + ":" + ("0" + now.getMinutes()).slice(-2)
-    + ":" + ("0" + now.getSeconds()).slice(-2);
+    + '-' + ('0' + (now.getMonth() + 1)).slice(-2)
+    + '-' + ('0' + now.getDate()).slice(-2)
+    + ' ' + ('0' + now.getHours()).slice(-2)
+    + ':' + ('0' + now.getMinutes()).slice(-2)
+    + ':' + ('0' + now.getSeconds()).slice(-2);
 }
 
 /* helper functions */

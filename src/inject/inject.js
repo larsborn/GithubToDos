ad6ff995cbbd2ce4453a8b7d5e21bfb177022979
David Badura <d.badura@gmx.de>
2017-04-11 19:34:55 +0200

    refactoring to reactjs

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -9,11 +9,11 @@ chrome.extension.sendMessage({}, function (response) {
 
 function init() {
   chrome.storage.onChanged.addListener(changes => {
-    render(changes.todos.newValue || []);
+    display(changes.todos.newValue || []);
   });
 
   chrome.storage.sync.get('todos', items => {
-    render(items.todos || []);
+    display(items.todos || []);
   });
 
   const target = document.querySelector('#js-repo-pjax-container');
@@ -24,56 +24,66 @@ function init() {
 
   const observer = new MutationObserver(function (mutations) {
     chrome.storage.sync.get('todos', items => {
-      render(items.todos || []);
+      display(items.todos || []);
     });
   });
 
-  observer.observe(target, {childList: true});
+  observer.observe(target, { childList: true });
 }
 
-function render(todos) {
-  renderButton(todos);
-  renderNavIcon(todos);
+function display(todos) {
+  displayButton(todos);
+  displayNavIcon(todos);
 }
 
-function renderButton(todos) {
+function displayButton(todos) {
   const sidebar = document.querySelector('#partial-discussion-sidebar');
 
   if (!sidebar) {
     return;
   }
 
-  let button = document.querySelector('.button-toggle-todo');
+  let mount = document.querySelector('#github-todos-sidebar');
 
-  if (!button) {
-    const buttonDiv = document.createElement('div');
-    buttonDiv.classList.add('thread-subscription-status', 'thread-todo-status');
+  if (!mount) {
+    mount = document.createElement('div');
+    mount.id = 'github-todos-sidebar';
 
-    button = document.createElement('button');
-    button.classList.add('btn', 'btn-sm', 'button-toggle-todo');
-    button.onclick = toggleTodo;
-
-    buttonDiv.appendChild(button);
-
-    const h3 = document.createElement('h3');
-    h3.classList.add('discussion-sidebar-heading');
-    h3.innerHTML = 'ToDos';
-
-    const outerDiv = document.createElement('div');
-    outerDiv.classList.add('discussion-sidebar-item');
-    outerDiv.classList.add('sidebar-notifications');
-    outerDiv.appendChild(h3);
-    outerDiv.appendChild(buttonDiv);
-    sidebar.insertBefore(outerDiv, document.getElementById('partial-users-participants'));
+    sidebar.insertBefore(mount, document.getElementById('partial-users-participants'));
   }
 
   const url = location.href;
   const index = todos.indexOf(url);
 
-  button.innerHTML = getSvg() + (index === -1 ? ' Add ToDo' : ' Remove ToDo');
+  render(renderButton(index === -1), mount);
 }
 
-function renderNavIcon(todos) {
+function renderButton(exists) {
+  const button = element('div', {
+    className: 'thread-subscription-status thread-todo-status'
+  },
+    element('button', {
+      className: 'btn btn-sm button-toggle-todo',
+      onClick: toggleTodo
+    },
+      renderSvg(),
+      exists ? ' Add ToDo' : ' Remove ToDo'
+    )
+  );
+
+  const h3 = element('h3', {
+    className: 'discussion-sidebar-heading'
+  }, 'ToDos')
+
+  return element('div', {
+    className: 'discussion-sidebar-item sidebar-notifications'
+  },
+    h3,
+    button
+  )
+}
+
+function displayNavIcon(todos) {
   const nav = document.querySelector('.header-nav.user-nav');
 
   if (!nav) {
@@ -84,80 +94,93 @@ function renderNavIcon(todos) {
 
   if (!navItem) {
     navItem = document.createElement('li');
-    navItem.classList.add('header-nav-item');
-    navItem.classList.add('header-nav-item-todo');
-    navItem.onclick = e => {
-      renderPage();
-      e.preventDefault();
-    };
+    navItem.classList.add('header-nav-item', 'header-nav-item-todo');
 
     nav.insertBefore(navItem, nav.childNodes[0]);
   }
 
-  navItem.innerHTML = `
-    <a href="#todos" 
-      class="header-nav-link notification-indicator tooltipped tooltipped-s js-notification-indicator">
-        ${todos.length > 0 ? '<span class="mail-status unread"></span>' : ''}
-        ${getSvg()}
-    </a>
-  `;
+  render(renderNavIcon(todos), navItem);
+}
+
+function renderNavIcon(todos) {
+  return element('a', {
+    href: '#todos',
+    className: 'header-nav-link notification-indicator tooltipped tooltipped-s js-notification-indicator',
+    onClick: e => {
+      renderPage();
+      e.preventDefault();
+    }
+  },
+    todos.length > 0 ? element('span', { className: 'mail-status unread' }) : null,
+    renderSvg()
+  );
 }
 
 function renderPage() {
   const container = document.querySelector('#js-repo-pjax-container, #js-pjax-container');
 
   chrome.storage.onChanged.addListener(function (changes) {
-    container.innerHTML = '';
-    container.appendChild(renderPageContent(changes.todos.newValue || []));
+    render(renderPageContent(changes.todos.newValue || []), container);
   });
 
   chrome.storage.sync.get('todos', items => {
-    container.innerHTML = '';
-    container.appendChild(renderPageContent(items.todos || []));
+    render(renderPageContent(items.todos || []), container);
   });
 }
 
 function renderPageContent(todos) {
 
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('container', 'page-content');
+  const list = todos.map(function (todo) {
 
-  const ul = document.createElement('ul');
-  todos.map(function (todo) {
-    const li = document.createElement('li');
-    const removeBtn = document.createElement('button');
-
-    removeBtn.onclick = () => {
-      chrome.storage.sync.get('todos', items => {
-        const todos = items.todos || [];
-        const pos = todos.indexOf(todo);
-        if (pos != -1) {
-          todos.splice(pos, 1);
-          chrome.storage.sync.set({
-            'todos': todos
-          });
+    const button = element(
+      'button',
+      {
+        className: 'btn btn-sm',
+        onClick: () => {
+          chrome.storage.sync.get('todos', items => {
+            const todos = items.todos || [];
+            const pos = todos.indexOf(todo);
+            if (pos != -1) {
+              todos.splice(pos, 1);
+              chrome.storage.sync.set({
+                'todos': todos
+              });
+            }
+          })
         }
-      });
-    };
+      },
+      'Done'
+    );
 
-    removeBtn.classList.add('btn');
-    removeBtn.classList.add('btn-sm');
-    removeBtn.innerHTML = 'Done';
+    const link = element(
+      'a',
+      {
+        style: {
+          paddingLeft: '0.5em',
+          href: todo
+        }
+      },
+      todo.replace('https://github.com/', '')
+    );
 
-    const a = document.createElement('a');
-    a.style.paddingLeft = '0.5em';
-    a.href = todo;
-    a.innerHTML = todo.replace('https://github.com/', '');
-
-    li.appendChild(removeBtn);
-    li.appendChild(a);
-    li.style.padding = '0.2em';
-    ul.appendChild(li)
+    return element(
+      'li',
+      {
+        key: todo,
+        style: { padding: '0.2em' }
+      },
+      link,
+      button
+    );
   });
 
-  wrapper.appendChild(ul);
-
-  return wrapper;
+  return element('div', { className: 'container page-content' },
+    element(
+      'ul',
+      {},
+      list
+    )
+  );
 }
 
 function toggleTodo() {
@@ -178,10 +201,27 @@ function toggleTodo() {
   });
 }
 
-function getSvg() {
-  return `
-    <svg class="octicon octicon-checklist" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-        <path fill-rule="evenodd" d="M16 8.5l-6 6-3-3L8.5 10l1.5 1.5L14.5 7 16 8.5zM5.7 12.2l.8.8H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h7c.55 0 1 .45 1 1v6.5l-.8-.8c-.39-.39-1.03-.39-1.42 0L5.7 10.8a.996.996 0 0 0 0 1.41v-.01zM4 4h5V3H4v1zm0 2h5V5H4v1zm0 2h3V7H4v1zM3 9H2v1h1V9zm0-2H2v1h1V7zm0-2H2v1h1V5zm0-2H2v1h1V3z"></path>
-     </svg>
-  `;
+function renderSvg() {
+  return element('svg', {
+    className: 'octicon octicon-checklist',
+    viewBox: '0 0 16 16',
+    version: '1.1',
+    width: 16,
+    height: 16
+  },
+    element('path', {
+      fillRule: 'evenodd',
+      d: 'M16 8.5l-6 6-3-3L8.5 10l1.5 1.5L14.5 7 16 8.5zM5.7 12.2l.8.8H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h7c.55 0 1 .45 1 1v6.5l-.8-.8c-.39-.39-1.03-.39-1.42 0L5.7 10.8a.996.996 0 0 0 0 1.41v-.01zM4 4h5V3H4v1zm0 2h5V5H4v1zm0 2h3V7H4v1zM3 9H2v1h1V9zm0-2H2v1h1V7zm0-2H2v1h1V5zm0-2H2v1h1V3z'
+    })
+  );
+}
+
+/* helper functions */
+
+function element(type, props, ...children) {
+  return React.createElement(type, props, ...children)
+}
+
+function render(element, mountNode) {
+  return ReactDOM.render(element, mountNode);;
 }
